2f044a71bce9ccdebf3bc4a590dba3cee49138a1
Anderson <abmelorj@gmail.com>
2022-05-31 18:11:14 -0300

    Ajustes para a aplicação e a API responderem pelo mesmo endereço: abmelo.com.br

--- spa-cfp/src/environments/environment.prod.ts
+++ spa-cfp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://abmelo.com.br/cfp/app/api',
-  auth: 'https://abmelo.com.br/cfp/app/auth'
+  api: 'https://abmelo.com.br/api',
+  auth: 'https://abmelo.com.br/auth'
 };
